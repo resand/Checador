75776122a34594e7b73435f95983f92a6a84f5e1
josecamal <jose.i.caamal.chable@gmail.com>
2014-07-19 14:41:31 -0100

    index

---
+++ indexcheacador.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="es">
+<head>
+	<title>Checador de entradas</title>
+</head>
+<body>
+<div>
+	<h3>Bienvenido</h3>
+
+	<label>Teclea tu clave</label><input type="text" name="clave" id="clave"/>
+	<input type="submit" name="entrada" id="entrada" value="Registrar entrada"/>
+
+	
+</div>
+	
+
+	
+
+	
+</body>
+
+
+</html>
